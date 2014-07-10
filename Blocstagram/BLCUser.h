86368221d5a411aa3a2385c0bed126648fcae10c
Jordan Hudgens <jordan@wow.com>
2014-07-10 17:32:12 -0500

    created a new model for users

---
+++ Blocstagram/BLCUser.h
@@ -0,0 +1,13 @@
+//
+//  BLCUser.h
+//  Blocstagram
+//
+//  Created by Jordan Hudgens on 7/10/14.
+//  Copyright (c) 2014 Jordan Hudgens. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BLCUser : NSObject
+
+@end
---
+++ Blocstagram/BLCUser.m
@@ -0,0 +1,13 @@
+//
+//  BLCUser.m
+//  Blocstagram
+//
+//  Created by Jordan Hudgens on 7/10/14.
+//  Copyright (c) 2014 Jordan Hudgens. All rights reserved.
+//
+
+#import "BLCUser.h"
+
+@implementation BLCUser
+
+@end
