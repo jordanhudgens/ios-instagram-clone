9a676734cb9732a22527e4087246237db6205aca
Jordan Hudgens <jordan@wow.com>
2014-07-11 17:45:42 -0500

    integrated formatting for the comment class

---
+++ Blocstagram/BLCMediaTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  BLCMediaTableViewCell.h
+//  Blocstagram
+//
+//  Created by Jordan Hudgens on 7/11/14.
+//  Copyright (c) 2014 Jordan Hudgens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BLCMedia;
+
+@interface BLCMediaTableViewCell : UITableViewCell
+
+@property (nonatomic, strong) BLCMedia *mediaItem;
+
+@end
---
+++ Blocstagram/BLCMediaTableViewCell.m
@@ -0,0 +1,107 @@
+//
+//  BLCMediaTableViewCell.m
+//  Blocstagram
+//
+//  Created by Jordan Hudgens on 7/11/14.
+//  Copyright (c) 2014 Jordan Hudgens. All rights reserved.
+//
+
+#import "BLCMediaTableViewCell.h"
+#import "BLCMedia.h"
+#import "BLCComment.h"
+#import "BLCUser.h"
+
+@interface BLCMediaTableViewCell ()
+
+@property (nonatomic, strong) UIImageView *mediaImageView;
+@property (nonatomic, strong) UILabel *usernameAndCaptionLabel;
+@property (nonatomic, strong) UILabel *commentLabel;
+
+@end
+
+static UIFont *lightFont;
+static UIFont *boldFont;
+static UIColor *usernameLabelGray;
+static UIColor *commentLabelGray;
+static UIColor *linkColor;
+static NSParagraphStyle *paragraphStyle;
+
+@implementation BLCMediaTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        self.mediaImageView = [[UIImageView alloc] init];
+        self.usernameAndCaptionLabel = [[UILabel alloc] init];
+        self.commentLabel = [[UILabel alloc] init];
+        self.commentLabel.numberOfLines = 0;
+        
+        for (UIView *view in @[self.mediaImageView, self.usernameAndCaptionLabel, self.commentLabel]) {
+            [self.contentView addSubview:view];
+        }
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
++ (void)load {
+    lightFont = [UIFont fontWithName:@"HelveticaNeue-Thin" size:11];
+    boldFont = [UIFont fontWithName:@"HelveticaNeue-Bold" size:11];
+    usernameLabelGray = [UIColor colorWithRed:0.933 green:0.933 blue:0.933 alpha:1];
+    commentLabelGray = [UIColor colorWithRed:0.898 green:0.898 blue:0.898 alpha:1];
+    linkColor = [UIColor colorWithRed:0.345 green:0.314 blue:0.427 alpha:1];
+    
+    NSMutableParagraphStyle *mutableParagraphStyle = [[NSMutableParagraphStyle alloc] init];
+    mutableParagraphStyle.headIndent = 20.0;
+    mutableParagraphStyle.firstLineHeadIndent = 20.0;
+    mutableParagraphStyle.tailIndent = -20.0;
+    mutableParagraphStyle.paragraphSpacingBefore = 5;
+    
+    paragraphStyle = mutableParagraphStyle;
+}
+
+- (NSAttributedString *) usernameAndCaptionString {
+    CGFloat usernameFontSize = 15;
+    
+    NSString *baseString = [NSString stringWithFormat:@"%@ %@", self.mediaItem.user.userName, self.mediaItem.caption];
+    
+    NSMutableAttributedString *mutableUsernameAndCaptionString = [[NSMutableAttributedString alloc] initWithString:baseString attributes:@{NSFontAttributeName : [lightFont fontWithSize:usernameFontSize], NSParagraphStyleAttributeName : paragraphStyle}];
+    
+    NSRange usernameRange = [baseString rangeOfString:self.mediaItem.user.userName];
+    
+    [mutableUsernameAndCaptionString addAttribute:NSFontAttributeName value:[boldFont fontWithSize:usernameFontSize] range:usernameRange];
+    [mutableUsernameAndCaptionString addAttribute:NSForegroundColorAttributeName value:linkColor range:usernameRange];
+    return mutableUsernameAndCaptionString;
+}
+
+- (NSAttributedString *) commentString {
+    NSMutableAttributedString *commentString = [[NSMutableAttributedString alloc] init];
+    
+    for (BLCComment *comment in self.mediaItem.comments) {
+        NSString *baseString = [NSString stringWithFormat:@"%@ %@\n", comment.from.userName, comment.text];
+        
+        NSMutableAttributedString *oneCommentString = [[NSMutableAttributedString alloc] initWithString:baseString attributes:@{NSFontAttributeName : lightFont, NSParagraphStyleAttributeName : paragraphStyle}];
+        
+        NSRange usernameRange = [baseString rangeOfString:comment.from.userName];
+        [oneCommentString addAttribute:NSFontAttributeName value:boldFont range:usernameRange];
+        [oneCommentString addAttribute:NSForegroundColorAttributeName value:linkColor range:usernameRange];
+        
+        [commentString appendAttributedString:oneCommentString];
+    }
+    
+    return commentString;
+}
+
+@end
